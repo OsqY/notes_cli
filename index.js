49cf1fe0f989954195127b572207a3ba1e8b472f
Tu Nombre <oscarjosehpc@gmail.com>
2023-12-29 19:54:02 -0600

    Revert "Revert "First commit""
    This reverts commit 31317a753e835c19c99e8e2874fa6dcac5c1c8b2.

---
+++ index.js
@@ -0,0 +1,4 @@
+#!/home/oscar/.nvm/versions/node/v20.10.0/bin/node           
+
+import './src/command.js'
+
---
+++ src/db.js
@@ -0,0 +1,5 @@
+import fs from 'node:fs/promises'
+
+const DB_PATH = new URL('../database.json', import.meta.url).pathname
+
+
